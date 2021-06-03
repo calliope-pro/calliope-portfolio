0065853e840c4d1deb752ae9760c2d249077b641
calliope <calliope183@gmail.com>
2021-06-04 06:04:22 +0900

    master

--- calliope_web/urls.py
+++ calliope_web/urls.py
@@ -5,16 +5,12 @@ app_name = 'calliope_web'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('profile/callback/', views.LineLinkView.as_view(), name='linelink'),
     path('contact/', views.ContactView.as_view(), name='contact'),
-    # path('login/', views.LoginWebView.as_view(), name='login'),
-    # path('login/testuser/', views.LoginTestuser.as_view(), name='login_testuser'),
-    # path('logout/', views.LogoutWebView.as_view(), name='logout'),
     path('bss/', views.BssListView.as_view(), name='bss_list'),
     path('bss/new/', views.BssCreateView.as_view(), name='bss_create'),
     path('bss/<int:pk>/', views.BssDetailView.as_view(), name='bss_detail'),
     path('bss/<int:pk>/update/', views.BssUpdateView.as_view(), name='bss_update'),
     path('bss/<int:pk>/delete/', views.BssDeleteView.as_view(), name='bss_delete'),
-    # path('signup/', views.SignUpView.as_view(), name='signup'),
-    # path('signup/<token>/', views.SignUpDoneView.as_view(), name='signup_done'),
     path('support/', views.SupportView.as_view(), name='support'),
 ]
--- calliope_web/views.py
+++ calliope_web/views.py
@@ -1,58 +1,34 @@
 import os
-import re
-from django.views.generic.edit import DeleteView, DeletionMixin
+import uuid
 
+import jwt
 import payjp
+import requests
 from calliope_bot.models import LineProfile
-from django.db.models import Q
 from django.conf import settings
 from django.contrib.auth import get_user_model, login
 from django.contrib.auth.mixins import UserPassesTestMixin
-from django.contrib.auth.views import LoginView, LogoutView
 from django.core.mail import send_mail
-from django.core.signing import BadSignature, SignatureExpired, dumps, loads
-from django.http.response import HttpResponseBadRequest
+from django.db.models import Q
 from django.shortcuts import redirect, render
-from django.template.loader import render_to_string
 from django.urls import reverse, reverse_lazy
 from django.views.generic import (CreateView, DetailView, FormView, ListView,
-                                  TemplateView, UpdateView)
-from django.views.generic.base import View
+                                  TemplateView, UpdateView, View)
+from django.views.generic.edit import DeleteView
 from payjp.error import PayjpException
 
-from .forms import BssForm, ContactForm, UserCreateForm
+from .forms import BssForm, ContactForm
 from .models import Bss
 
+LINE_REDIRECT_URL = os.environ['LINE_REDIRECT_URL']
+LINE_CHANNEL_ID = os.environ['LINE_CHANNEL_ID']
+LINE_CHANNEL_SECRET = os.environ['LINE_CHANNEL_SECRET']
+LINE_RANDOM_STATE = str(uuid.uuid1())
+LINE_NONCE = str(uuid.uuid4())
 
 # Create your views here.
 class HomeView(TemplateView):
     template_name = "calliope_web/home.html"
-
-
-# class LoginWebView(LoginView):
-#     template_name = 'calliope_web/login.html'
-#     redirect_authenticated_user = True
-    
-#     def get_success_url(self):
-#         return reverse('calliope_web:home')
-    
-#     def post(self, request, *args, **kwargs):
-#         form = self.get_form()
-#         request_username = request.POST.get('username')
-#         for allowed_login_user in settings.ALLOWED_LOGIN_USERS:
-#             if re.fullmatch(allowed_login_user, request_username):
-#                 break
-#         else:
-#             form.add_error('username', 'usernameはuser〇〇のみ有効です')
-
-#         if form.is_valid():
-#             return self.form_valid(form)
-#         else:
-#             return self.form_invalid(form)
-
-
-# class LogoutWebView(LogoutView):
-#     pass
 
 
 class ContactView(FormView):
@@ -79,9 +55,13 @@ class ContactView(FormView):
 
 class ProfileView(TemplateView):
     template_name = 'calliope_web/profile.html'
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['channel_id'] = LINE_CHANNEL_ID
+        context['redirect_url'] = LINE_REDIRECT_URL
+        context['random_state'] = LINE_RANDOM_STATE
+        context['nonce'] = LINE_NONCE
         lineprofile, _ = LineProfile.objects.get_or_create(user=self.request.user)
         if lineprofile.line_id:
             context['line_id'] = lineprofile.line_id
@@ -149,64 +129,6 @@ class BssListView(ListView):
         return queryset
 
 
-# class SignUpView(CreateView):
-#     template_name = 'calliope_web/signup.html'
-#     form_class = UserCreateForm
-#     timeout_minutes = 30
-    
-#     def form_valid(self, form):
-#         user = form.save(commit=False)
-#         user.is_active = False
-#         user.save()
-        
-#         current_url = self.request.build_absolute_uri()
-#         token = dumps(user.pk)
-#         sub = f'{user.username}さん、仮登録が完了しました。'
-#         context = {
-#             'token': token,
-#             'current_url': current_url,
-#             'username': user.username,
-#             'password': len(form.cleaned_data['password2']) * '*',
-#             'timeout_minutes': self.timeout_minutes,
-#         }
-#         if settings.DEBUG:
-#             context['password'] = form.cleaned_data['password2']
-#         body = render_to_string('calliope_web/email_body.txt', context)
-#         user.email_user(sub, body)
-#         context = {
-#             'form':self.get_form_class(),
-#             'done':True
-#         }
-#         return render(self.request, 'calliope_web/signup.html', context)
-
-
-# class LoginTestuser(View):
-#     def get(self, request, *args, **kwargs):
-#         user = get_user_model().objects.get(username='testuser')
-#         login(request, user)
-#         return redirect('calliope_web:home')
-
-
-# class SignUpDoneView(TemplateView):
-#     template_name = "calliope_web/signup_done.html"
-    
-#     def get(self, request, *args, **kwargs):
-#         token = kwargs.get('token')
-#         try:
-#             user_pk = loads(token, max_age=60*SignUpView.timeout_minutes)
-#         except (SignatureExpired, BadSignature):
-#             return HttpResponseBadRequest()
-        
-#         try:
-#             user = get_user_model().objects.get(pk=user_pk)
-#         except get_user_model().DoesNotExist:
-#             return HttpResponseBadRequest()
-#         else:
-#             user.is_active = True
-#             user.save()
-#             return render(request, self.template_name, {'user':user})
-
-
 class BssDetailView(DetailView):
     model = Bss
     template_name = "calliope_web/bss_detail.html"
@@ -238,3 +160,41 @@ class BssDeleteView(UserPassesTestMixin, DeleteView):
     def test_func(self):
         user = self.request.user
         return user == self.model.objects.select_related('author').get(pk=self.kwargs['pk']).author
+
+
+class LineLinkView(View):
+    URI_ACCESS_TOKEN = 'https://api.line.me/oauth2/v2.1/token'
+    HEADERS = {"Content-Type": "application/x-www-form-urlencoded"}
+
+    def get(self, request, *args, **kwargs):
+        request_code = request.GET.get("code")
+        data_params = {
+            "grant_type": "authorization_code",
+            "code": request_code,
+            "redirect_uri": LINE_REDIRECT_URL,
+            "client_id": LINE_CHANNEL_ID,
+            "client_secret": LINE_CHANNEL_SECRET
+        }
+
+        response_post = requests.post(self.URI_ACCESS_TOKEN, headers=self.HEADERS, data=data_params)
+
+        json_loads = response_post.json()
+        line_id_token = json_loads["id_token"]
+        line_profile = jwt.decode(line_id_token,
+                                LINE_CHANNEL_SECRET,
+                                audience=LINE_CHANNEL_ID,
+                                issuer='https://access.line.me',
+                                algorithms=['HS256']
+        )
+        
+        if LINE_NONCE != line_profile.get('nonce'):
+            raise RuntimeError('invalid nonce')
+
+        user = request.user
+        user_line_profile = LineProfile.objects.get(user=user)
+        user_line_profile.line_id = line_profile['sub']
+        user_line_profile.line_icon_url = line_profile['picture']
+        user_line_profile.line_name = line_profile['name']
+        user_line_profile.save()
+
+        return redirect('calliope_web:profile')
