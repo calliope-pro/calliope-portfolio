b6c53bca54e983fab284b0a48f1fd886b3c2ad93
calliope <calliope183@gmail.com>
2021-08-12 04:15:50 +0900

    master

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+try:
+    from .local_settings import DEBUG
+except ImportError:
+    DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
