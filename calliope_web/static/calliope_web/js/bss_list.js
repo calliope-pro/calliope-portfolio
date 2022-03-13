f5e66e6e6ce742e307d6c94c4eb1800d1aa1cd42
calliope <calliope183@gmail.com>
2022-03-14 04:07:15 +0900

    delete jQuery, add elevation

--- calliope_web/static/calliope_web/js/bss_list.js
+++ calliope_web/static/calliope_web/js/bss_list.js
@@ -1,6 +1,19 @@
-$(function(){
-    $('#confirm_delete').on('click', function() {
-        let flag = confirm('削除しますか？');
-        return flag;
+(function () {
+    document.getElementById('js_confirm_delete').addEventListener('click', function (e) {
+        const flag = confirm('削除しますか？');
+        if (!flag) {
+            e.preventDefault();
+        }
     });
-});
+})();
+
+(function () {
+    document.querySelectorAll('.card').forEach((element) => {
+        element.addEventListener('mouseover', () => {
+            element.classList.add('shadow-lg');
+        });
+        element.addEventListener('mouseout', () => {
+            element.classList.remove('shadow-lg');
+        });
+    });
+})();
